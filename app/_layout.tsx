c8168b76683775b1332faa2c2e87982534626525
Fran Perea <franciscopereatora@gmail.com>
2025-05-24 12:42:14 +0200

    feat: update global layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,48 @@
-import { ScreenContent } from 'components/ScreenContent';
+// app/_layout.tsx
+import { Slot, router } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SplashScreen from '../components/SplashScreen';
+import * as SecureStore from 'expo-secure-store';
+import { restoreSession } from '../lib/auth';
 import { StatusBar } from 'expo-status-bar';
-import SplashScreen from 'components/SplashScreen';
-import EmailScreen from 'app/(auth)/EmailScreen';
-
 import '../global.css';
-import { useState } from 'react';
 
 export default function RootLayout() {
   const [isAppReady, setIsAppReady] = useState(false);
+  const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    const bootstrap = async () => {
+      const loggedIn = await restoreSession();
+      if (!loggedIn) {
+        router.replace('/(auth)/EmailScreen');
+        return setIsAppReady(true);
+      }
+
+      const hasSeenOnboarding = await SecureStore.getItemAsync('hasSeenOnboarding');
+      if (!hasSeenOnboarding) {
+        router.replace('/(auth)/OnboardingScreen');
+      } else {
+        router.replace('/(tabs)/Chat');
+      }
+
+      setIsAppReady(true);
+    };
+
+    bootstrap();
+  }, []);
+
   if (!isAppReady) {
     return (
       <SplashScreen onFinish={(isCancelled: boolean) => !isCancelled && setIsAppReady(true)} />
     );
   }
+
   return (
-    <>
-      <EmailScreen />
+    <QueryClientProvider client={queryClient}>
+      <Slot />
       <StatusBar style="auto" />
-    </>
+    </QueryClientProvider>
   );
 }
