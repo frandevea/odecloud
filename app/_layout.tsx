d6c283f3c1b1fa4a2a50dfa934fbd3e1afc2f5da
Fran Perea <franciscopereatora@gmail.com>
2025-05-25 21:47:17 +0200

    feat: install rnr

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,59 @@
 import { Slot } from 'expo-router';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import '../global.css';
 import { StatusBar } from 'expo-status-bar';
+import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '@/lib/constants';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import '@/global.css';
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
   const [queryClient] = useState(() => new QueryClient());
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add('bg-background');
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Slot />
-      <StatusBar style="auto" />
-    </QueryClientProvider>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <QueryClientProvider client={queryClient}>
+        <Slot />
+        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
