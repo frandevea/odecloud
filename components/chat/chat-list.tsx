61f06029c48bf8fa76fbb3670fff31bb32236c5e
Fran Perea <franciscopereatora@gmail.com>
2025-05-26 20:15:36 +0200

    feat: create chat list component

---
+++ components/chat/chat-list.tsx
@@ -0,0 +1,25 @@
+import { FlatList } from 'react-native';
+import ChatListItem from './chat-list-item';
+import { Chat } from '@/types/chat';
+import ChatSearchInput from './chat-search-input'; // asegúrate de importar correctamente
+
+interface ChatListProps {
+  chats: Chat[];
+  userId: string;
+  onSelectChat: (chatId: string) => void;
+}
+
+export default function ChatList({ chats, userId, onSelectChat }: ChatListProps) {
+  return (
+    <FlatList
+      data={chats}
+      keyExtractor={(item) => item._id}
+      contentContainerStyle={{ paddingHorizontal: 8, paddingTop: 12 }}
+      renderItem={({ item }) => (
+        <ChatListItem chat={item} userId={userId} onPress={() => onSelectChat(item._id)} />
+      )}
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={<ChatSearchInput />}
+    />
+  );
+}
