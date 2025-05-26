d2e48b363a9341acb5dff62a014b62c194ea5295
Fran Perea <franciscopereatora@gmail.com>
2025-05-26 12:24:35 +0200

    feat: update padding on chat screen

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,12 @@
 import { View } from 'react-native';
 import ChatHeader from '@/components/chat/chat-header';
+import ChatEmptyState from '@/components/chat/empty-state';
 
 export default function ChatScreen() {
   return (
-    <View className="flex-1 bg-background">
+    <View className="flex-1 pb-32 bg-background">
       <ChatHeader userName="David" isOnline />
+      <ChatEmptyState />
     </View>
   );
 }
