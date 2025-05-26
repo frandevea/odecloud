9b60d1a0d6538c3a385a369b9b457a6c14cd9719
Fran Perea <franciscopereatora@gmail.com>
2025-05-26 12:24:15 +0200

    feat: create emptystate for chat page

---
+++ components/chat/empty-state.tsx
@@ -0,0 +1,35 @@
+import { View } from 'react-native';
+import { Text } from '@/components/ui/text';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'expo-router';
+import { MessageSquareDashed } from '@/lib/icons/MessageSquareDashed';
+import { ArrowRight } from '@/lib/icons/ArrowRight';
+
+export default function ChatEmptyState() {
+  const router = useRouter();
+
+  const handleGoToFeed = () => {
+    router.push('/(tabs)/feed'); // Asumiendo que la ruta del feed es '/feed'
+  };
+
+  return (
+    <View className="items-center justify-center flex-1 p-6">
+      <View className="p-4 mb-4">
+        <MessageSquareDashed size={48} className="text-primary" />
+      </View>
+
+      <Text className="mb-2 text-2xl font-semibold text-foreground">No chat</Text>
+      <Text className="max-w-xs text-justify text-muted-foreground">
+        You don&apos;t have any chatroom yet.
+      </Text>
+      <Text className="max-w-xs mb-8 text-justify text-muted-foreground">
+        Start meeting peers in OdeSocial.
+      </Text>
+
+      <Button variant="action" size="lg" className="flex flex-row w-full" onPress={handleGoToFeed}>
+        <Text className="mr-2 text-primary-foreground">Go to the feed</Text>
+        <ArrowRight size={18} className="text-primary-foreground" />
+      </Button>
+    </View>
+  );
+}
---
+++ lib/icons/MessageSquareDashed.tsx
@@ -0,0 +1,4 @@
+import { MessageSquareDashed } from 'lucide-react-native';
+import { iconWithClassName } from './iconWithClassName';
+iconWithClassName(MessageSquareDashed);
+export { MessageSquareDashed };
