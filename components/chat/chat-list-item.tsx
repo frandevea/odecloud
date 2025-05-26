93ad679f5674d6ba39e5f7f9ca92d9fd4962a458
Fran Perea <franciscopereatora@gmail.com>
2025-05-27 01:02:25 +0200

    feat: update chat list item

--- components/chat/chat-list-item.tsx
+++ components/chat/chat-list-item.tsx
@@ -2,6 +2,7 @@ import { View, Pressable } from 'react-native';
 import { Text } from '@/components/ui/text';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { getRelativeTimestamp } from '@/lib/utils';
+import { GroupAvatar } from '@/components/group-avatar';
 
 interface ChatListItemProps {
   chat: any;
@@ -10,12 +11,17 @@ interface ChatListItemProps {
 }
 
 export default function ChatListItem({ chat, userId, onPress }: ChatListItemProps) {
-  const otherMembers = chat.data.members.filter((m: any) => m._id !== userId);
+  const members = chat.data.members ?? [];
+  const otherMembers = members.filter((m: any) => m._id !== userId);
   const otherUser = otherMembers[0];
+  const isGroup = members.length > 2;
+
   const lastMessage = chat.data.messages.data?.[0];
 
-  const userName =
-    `${otherUser?.profile?.firstName ?? ''} ${otherUser?.profile?.lastName ?? ''}`.trim();
+  const userName = isGroup
+    ? (chat.title ?? 'Group')
+    : `${otherUser?.profile?.firstName ?? ''} ${otherUser?.profile?.lastName ?? ''}`.trim();
+
   const userInitials = (
     (otherUser?.profile?.firstName?.[0] ?? '') + (otherUser?.profile?.lastName?.[0] ?? '')
   ).toUpperCase();
@@ -24,17 +30,31 @@ export default function ChatListItem({ chat, userId, onPress }: ChatListItemProp
     ? getRelativeTimestamp(lastMessage.createdAt)
     : null;
 
+  // Para el avatar
+  const avatars = members.map((m: any) => {
+    const first = m.profile?.firstName?.[0] ?? '';
+    const last = m.profile?.lastName?.[0] ?? '';
+    return {
+      uri: m.profile?.avatar?.secureUrl,
+      initials: (first + last).toUpperCase(),
+    };
+  });
+
   return (
     <Pressable onPress={onPress} className="flex-row items-center gap-3 px-3 ">
       <View className="py-4">
-        <Avatar alt={userName || 'User avatar'} className="h-14 w-14">
-          <AvatarImage source={{ uri: otherUser?.profile?.avatar?.secureUrl }} />
-          <AvatarFallback>
-            {userInitials ? (
-              <Text className="text-xs font-medium text-muted-foreground">{userInitials}</Text>
-            ) : null}
-          </AvatarFallback>
-        </Avatar>
+        {isGroup ? (
+          <GroupAvatar avatars={avatars} />
+        ) : (
+          <Avatar alt={userName || 'User avatar'} className="h-14 w-14">
+            <AvatarImage source={{ uri: otherUser?.profile?.avatar?.secureUrl }} />
+            <AvatarFallback>
+              {userInitials ? (
+                <Text className="text-xs font-medium text-muted-foreground">{userInitials}</Text>
+              ) : null}
+            </AvatarFallback>
+          </Avatar>
+        )}
       </View>
       <View className="flex-1 h-full pt-4 border-b border-gray-100">
         <View className="flex-row items-center justify-between">
